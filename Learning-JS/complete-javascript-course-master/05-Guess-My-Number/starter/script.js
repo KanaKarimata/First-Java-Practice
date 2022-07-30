7d62de44f86a5b04f369684a0ab371b193ba6e5d
KanaKarimata <kana.lt22@gmail.com>
2022-07-30 23:49:38 +0900

    finish part7-77

--- Learning-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Learning-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -11,7 +11,8 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(`.number`).textContent = secretNumber;
+let highScore = 0;
+document.querySelector(`.number`).textContent = `?`;
 
 document.querySelector(`.check`).addEventListener(`click`, function(){
   const guess = Number(document.querySelector(`.guess`).value);
@@ -23,10 +24,16 @@ document.querySelector(`.check`).addEventListener(`click`, function(){
     //When player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `Correct Number🐵!`;
-
+    
+    //change CSS
     document.querySelector(`body`).style.backgroundColor = '#60b347';
-
     document.querySelector(`.number`).style.width = `30rem`;
+    
+    //high score
+    if(score > highScore) {
+      highScore = score;
+      document.querySelector(`.highscore`).textContent = highScore;
+    }
 
     //When guess is too high
   } else if (guess > secretNumber) {
