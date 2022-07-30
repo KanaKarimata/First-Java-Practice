2baccd389c1cb75cb022ba2a3a88285f5212da47
KanaKarimata <kana.lt22@gmail.com>
2022-07-30 22:56:49 +0900

    finish part7-74

--- Learning-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Learning-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -9,11 +9,37 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(`.number`).textContent = secretNumber;
+
 document.querySelector(`.check`).addEventListener(`click`, function(){
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
 
   if(!guess) {
     document.querySelector(`.message`).textContent = `🚫 No number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `Correct Number🐵!`;
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector(`.message`).textContent = `❗️Too High!`;
+    score--;
+    document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `☠️ You lost the game!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if(score > 1) {
+    document.querySelector(`.message`).textContent = `❕Too Low!`;
+    score--;
+    document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `☠️ You lost the game!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    }
   }
 });
