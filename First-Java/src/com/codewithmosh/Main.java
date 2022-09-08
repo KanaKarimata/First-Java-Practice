8e948733e840be3eeee166f0bc10b46db203f0ee
KanaKarimata <kana.lt22@gmail.com>
2022-09-08 22:08:52 +0900

    First Commit

--- First-Java/src/com/codewithmosh/Main.java
+++ First-Java/src/com/codewithmosh/Main.java
@@ -1,8 +1,17 @@
 package com.codewithmosh;
 
 public class Main {
-
+//{}の中身はブロック
+//Javaのプログラムを作ることはクラスを作ること
+//クラス名とプログラムのファイル名を合わせること
     public static void main(String[] args) {
-        //write your code
+        //処理のまとまりをメソッドという
+        //Javaを実行するときに最初に実行されるメソッドはmainメソッド
+        //;を打つ
+        System.out.println("Javaを楽しもう！"); //画面表示
+        //わかりやすいプログラムの書き方３つ
+        //①メモ書きを残すこと＝コメント
+        //１行のコメント
+        /*複数行のコメント*/
     }
 }
