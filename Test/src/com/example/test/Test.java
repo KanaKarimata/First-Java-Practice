dcc8780df2e6456c2ce2f6df804085bd9fb2e733
KanaKarimata <kana.lt22@gmail.com>
2022-09-11 12:10:22 +0900

    Reference types

--- Test/src/com/example/test/Test.java
+++ Test/src/com/example/test/Test.java
@@ -1,11 +1,16 @@
 package com.example.test;
 
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
-        byte age = 30;
+        /* byte age = 30;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
         char letter = 'A';
-        boolean isEligible = false;
+        boolean isEligible = false;*/
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
