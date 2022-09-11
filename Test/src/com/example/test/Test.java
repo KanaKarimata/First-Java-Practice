64333d4b8a2898cdadb264e2902737c1975ed572
KanaKarimata <kana.lt22@gmail.com>
2022-09-11 14:26:57 +0900

    Primitive types vs Reference types

--- Test/src/com/example/test/Test.java
+++ Test/src/com/example/test/Test.java
@@ -1,16 +1,29 @@
 package com.example.test;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
+
         /* byte age = 30;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
         char letter = 'A';
         boolean isEligible = false;*/
-        byte age = 30;
+
+        /* byte age = 30;
         Date now = new Date();
-        System.out.println(now);
+        System.out.println(now);*/
+
+        /*byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);*/
+
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
