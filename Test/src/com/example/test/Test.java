b86b145c1cae6995c60cf48780b4244a2c1e2e22
KanaKarimata <kana.lt22@gmail.com>
2022-09-15 18:01:04 +0900

    finish Part3

--- Test/src/com/example/test/Test.java
+++ Test/src/com/example/test/Test.java
@@ -198,7 +198,65 @@ public class Test {
         age % 2 = 1;
         name.equal("湊");*/
 
+        /*for(int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++){
+                System.out.print(i * j);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
 
+        int isHungry = 1;
+        String food = "Ramen";
+        System.out.println("こんにちは");
+        if (isHungry == 0) {
+            System.out.println("もうお腹いっぱいです");
+        } else {
+            System.out.println("腹ペコです");
+            System.out.println(food + "をいただきます");
+        }
+        System.out.println("ご馳走様でした");*/
+
+        /*boolean tenki = false;
+        if (tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("DVDを見ます");
+            System.out.println("寝ます");
+        }*/
+
+        /*System.out.print("【メニュー】1：検索　2：登録　3：削除　4；変更>");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch(selected) {
+            case 1:
+                System.out.println("検索します");
+                break;
+            case 2:
+                System.out.println("登録します");
+                break;
+            case 3:
+                System.out.println("削除します");
+                break;
+            case 4:
+                System.out.println("変更します");
+                break;
+            default:
+        }*/
+
+        System.out.println("【数当てゲーム】");
+        int ans = new java.util.Random().nextInt(10);
+        for (int i = 1; i < 5; i++) {
+            System.out.println("0〜9の数字を入力してください");
+            int num = new java.util.Scanner(System.in).nextInt();
+            if (ans == num) {
+                System.out.println("あたり！");
+                break;
+            } else {
+                System.out.println("違います");
+            }
+        }
+        System.out.println("ゲームを終了します");
 
     }
 }
